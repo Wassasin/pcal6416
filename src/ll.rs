01079bdc2e26d094a99aa8d088308dab0ee3a128
Wouter Geraedts <git@woutergeraedts.nl>
2025-05-09 19:42:27 +0200

    Added sync ll

--- src/ll.rs
+++ src/ll.rs
@@ -1,5 +1,4 @@
-use device_driver::AsyncRegisterInterface;
-use embedded_hal_async::i2c::I2c;
+use device_driver::{AsyncRegisterInterface, RegisterInterface};
 
 use crate::Address;
 
@@ -12,7 +11,7 @@ pub struct Interface<I2C> {
 
 impl<I2C, E> AsyncRegisterInterface for Interface<I2C>
 where
-    I2C: I2c<Error = E>,
+    I2C: embedded_hal_async::i2c::I2c<Error = E>,
 {
     type Error = E;
     type AddressType = u8;
@@ -43,10 +42,38 @@ where
     }
 }
 
-impl<I2C, E> Interface<I2C>
+impl<I2C, E> RegisterInterface for Interface<I2C>
 where
-    I2C: I2c<Error = E>,
+    I2C: embedded_hal::i2c::I2c<Error = E>,
 {
+    type Error = E;
+    type AddressType = u8;
+
+    fn write_register(
+        &mut self,
+        address: Self::AddressType,
+        _size_bits: u32,
+        data: &[u8],
+    ) -> Result<(), Self::Error> {
+        let mut buf = [0u8; MAX_TRANSACTION_SIZE];
+        buf[0] = address;
+        buf[1..data.len() + 1].copy_from_slice(data);
+        let buf = &buf[0..data.len() + 1];
+
+        self.i2c.write(self.address.addr(), buf)
+    }
+
+    fn read_register(
+        &mut self,
+        address: Self::AddressType,
+        _size_bits: u32,
+        data: &mut [u8],
+    ) -> Result<(), Self::Error> {
+        self.i2c.write_read(self.address.addr(), &[address], data)
+    }
+}
+
+impl<I2C> Interface<I2C> {
     pub fn new(i2c: I2C, address: Address) -> Self {
         Self { i2c, address }
     }
